fca8b9d3bb22a4bb3a0ba4cf4ae46c8d9f4cd6f9
Alex <a.derzhko@yandex.ru>
2021-05-19 17:08:30 +0300

    delete detecte motion

--- webstreaming.py
+++ webstreaming.py
@@ -39,38 +39,38 @@ def detect_motion(frameCount):
     # initialize the motion detector and the total number of frames
     # read thus far
     md = SingleMotionDetector()
-    # total = 0
-    # # loop over frames from the video stream
-    # while True:
-    #     frame = vs.read()
-    #     frame = imutils.resize(frame, width=400)
-    #     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #     gray = cv2.GaussianBlur(gray, (7, 7), 0)
-    #     # grab the current timestamp and draw it on the frame
-    #     timestamp = datetime.datetime.now()
-    #     cv2.putText(frame, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-    #                 cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-    #     # if the total number of frames has reached a sufficient
-    #     # number to construct a reasonable background model, then
-    #     # continue to process the frame
-    #     if total > frameCount:
-    #         #detect motion in the image
-    #         motion = md.detect(gray)
-    #         # check to see if motion was found in the frame
-    #         if motion is not None:
-    #             # unpack the tuple and draw the box surrounding the
-    #             # "motion area" on the output frame
-    #             (thresh, (minX, minY, maxX, maxY)) = motion
-    #             cv2.rectangle(frame, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
-    #
-    #     # update the background model and increment the total number
-    #     # of frames read thus far
-    #     md.update(gray)
-    #     total += 1
-    #     # acquire the lock, set the output frame, and release the
-    #     # lock
-    #     with lock:
-    #         outputFrame = frame.copy()
+    total = 0
+    # loop over frames from the video stream
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        # grab the current timestamp and draw it on the frame
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        # if the total number of frames has reached a sufficient
+        # number to construct a reasonable background model, then
+        # continue to process the frame
+        if total > frameCount:
+            #detect motion in the image
+            motion = md.detect(gray)
+            # check to see if motion was found in the frame
+            if motion is not None:
+                # unpack the tuple and draw the box surrounding the
+                # "motion area" on the output frame
+                (thresh, (minX, minY, maxX, maxY)) = motion
+                cv2.rectangle(frame, (minX, minY), (maxX, maxY), (0, 0, 255), 2)
+
+        # update the background model and increment the total number
+        # of frames read thus far
+        md.update(gray)
+        total += 1
+        # acquire the lock, set the output frame, and release the
+        # lock
+        with lock:
+            outputFrame = frame.copy()
 
 
 def generate():
